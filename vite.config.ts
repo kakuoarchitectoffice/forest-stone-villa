c829af83b40f57c048f554291beb417ffd10bd18
kakuo-architect-office <eiji0228075@gmail.com>
2026-06-26 20:06:04 +0900

    fix: configure site for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/forest-stone-villa/",
+  base: "/",
   plugins: [react()],
 });
